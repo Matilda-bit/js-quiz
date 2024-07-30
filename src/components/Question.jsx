98dbbf2d2072938599f3a7423f0f68acf5f438e8
Matilda-bit <polinaovras@gmail.com>
2024-07-30 21:54:49 +0300

    move Question to separate component

---
+++ src/components/Question.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+import QuestionTimer from './QuestionTimer.jsx';
+import Answers from './Answers.jsx';
+import QUESTIONS from '../questions.js';
+
+export default function Question({
+  index,
+  onSelectAnswer,
+  onSkipAnswer,
+}) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null
+    })
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer
+      })
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = '';
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer) {
+    answerState = 'answered';
+  }
+
+  return (
+    <div id="question">
+      <QuestionTimer 
+        timeout={20000} 
+        onTimeout={onSkipAnswer} 
+        />
+      <h2>{QUESTIONS[index].text}</h2>
+      {QUESTIONS[activeQuestionIndex].code && 
+        <pre className="jscode">{QUESTIONS[activeQuestionIndex].code}</pre>}
+      <Answers
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+      />
+    </div>
+  );
+}
