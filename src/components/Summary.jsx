4b9ae62a2fff78c02b43d0a7b5e9b193916d90bc
Matilda-bit <polinaovras@gmail.com>
2024-09-10 00:11:27 +0300

    support different structure of answer

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -7,65 +7,32 @@ export default function Summary({ userAnswers }) {
   const correctAnswers = userAnswers.filter(
     
     (answer, index) => { 
-      //polina
       const correctIndex = QUESTIONS[index].correctAnswer; //always return index of correct answer - can be an array
-      if(Array.isArray(correctIndex)) {
-        console.log("correctIndex it is an array");
-        console.log(correctIndex);
-        console.log("index" + index);
+
+      if(answer === null) {
+        return false;
       }
 
-
-        let ans;
-        let isCorrect;
-        let correctAns;
+      let ans;
+      let isCorrect;
+      let correctAns;
       // Check if correctAnswer is an array
-        if (Array.isArray(correctIndex)) {
-          // Check if the selected answer is in the array of correct answers
-          isCorrect = correctIndex.some(ca => {
-              correctAns = QUESTIONS[index].answers[ca].label ? QUESTIONS[index].answers[ca].label : QUESTIONS[index].answers[ca];
-              ans = answer.label ? answer.label : answer;
-              return ans === correctAns;
-          });
-      } else if (answer !== null) {
+      if (Array.isArray(correctIndex)) {
+        // Check if the selected answer is in the array of correct answers
+        isCorrect = correctIndex.some(ca => {
+            correctAns = QUESTIONS[index].answers[ca].label ? QUESTIONS[index].answers[ca].label : QUESTIONS[index].answers[ca];
+            ans = answer.label ? answer.label : answer;
+            return ans === correctAns;
+        });
+      } else {
           correctAns = QUESTIONS[index].answers[correctIndex].label ? QUESTIONS[index].answers[correctIndex].label : QUESTIONS[index].answers[correctIndex];
           ans = answer.label ? answer.label : answer;
           isCorrect = correctAns === ans;
       }
 
-  
-      // console.log("index of answer:");
-      // // console.log(answer);
-      // console.log(index);
-      // //correctAns = QUESTIONS[index].answers[correctIndex].label ? QUESTIONS[index].answers[correctAnswer].label : QUESTIONS[index].answers[correctAnswer];
-        
-      // //console.log(QUESTIONS[index].answers[correctIndex]);
-      // if (Array.isArray(answer)) {
-      //   console.log("aswer this is an array!");
-      // }
-
-
-      // if(answer === null){
-
-      // }
-      // else if (typeof answer === "object" && answer !== null) {
-      //   console.log("answer this is an object!");
-      //   console.log(JSON.stringify(answer));
-      //   console.log("try to compere with");
-      //   console.log(QUESTIONS[index].answers[correctIndex]);
-      //   console.log(answer === QUESTIONS[index].answers[correctIndex]);
-
-      // }
-
       return isCorrect;
-      
-      //return answer === QUESTIONS[index].answers[correctIndex];
-      
     }
   );
-
-  // console.log(correctAnswers);
-  // console.log(correctAnswers);
 
   const skippedAnswersShare = Math.round(
     (skippedAnswers.length / userAnswers.length) * 100
@@ -97,30 +64,32 @@ export default function Summary({ userAnswers }) {
         {userAnswers.map((answer, index) => {
           let cssClass = 'user-answer';
           const correctIndex = QUESTIONS[index].correctAnswer;
-          console.log(answer);
 
           if (answer === null) {
             cssClass += ' skipped';
-          } else if (typeof answer === "object") {
-            console.log(answer.label);
-            console.log(answer.code);
-            if()
-            //cssClass += ' correct';
-
-          } else if (answer.label) {
-            console.log(answer.label);
-            //cssClass += ' correct';
-          } else if (Array.isArray(QUESTIONS[index].answers[correctIndex])) {
-            console.log("QUESTIONS[index].answers[correctIndex]");
-            if(QUESTIONS[index].answers[correctIndex]) {
-              
-            }
-            //cssClass += ' correct';
-            //here answer it is a problem
-          }  else if (answer === QUESTIONS[index].answers[correctIndex]) {
-            cssClass += ' correct';
           } else {
-            cssClass += ' wrong';
+            let ans;
+            let isCorrect;
+            let correctAns;
+            // Check if correctAnswer is an array
+            if (Array.isArray(correctIndex)) {
+              // Check if the selected answer is in the array of correct answers
+              isCorrect = correctIndex.some(ca => {
+                  correctAns = QUESTIONS[index].answers[ca].label ? QUESTIONS[index].answers[ca].label : QUESTIONS[index].answers[ca];
+                  ans = answer.label ? answer.label : answer;
+                  return ans === correctAns;
+              });
+            } else if (answer !== null) {
+                correctAns = QUESTIONS[index].answers[correctIndex].label ? QUESTIONS[index].answers[correctIndex].label : QUESTIONS[index].answers[correctIndex];
+                ans = answer.label ? answer.label : answer;
+                isCorrect = correctAns === ans;
+            }
+            
+            if (isCorrect) {
+              cssClass += ' correct';
+            } else {
+              cssClass += ' wrong';
+            }
           }
 
           return (
@@ -129,7 +98,23 @@ export default function Summary({ userAnswers }) {
               <p className="question">{QUESTIONS[index].text}</p>
               {QUESTIONS[index].code && 
                 <pre className="jscode">{QUESTIONS[index].code}</pre>}
-              <p className={cssClass}>{answer ?? 'Skipped'}</p>
+              <p className={cssClass}>
+                {answer ? (answer.label ?
+                (<span>{answer.label}</span>) 
+                :
+                answer
+                )
+                : 
+                'Skipped'}
+                </p>
+
+                {
+                  answer && answer.code ?
+                  (<>
+                  <pre className="jscode">{answer.code}</pre></>) 
+                  :
+                  null
+                }
             </li>
           );
         })}
